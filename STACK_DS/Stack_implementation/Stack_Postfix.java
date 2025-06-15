20ca01b97e6ff1914ec4f45697296ff36b40313f
Nishanthkb10 <nishanth.kb.10@gmail.com>
2025-06-15 10:11:21 +0530

    Update Stack_Postfix.java

--- STACK_DS/Stack_implementation/Stack_Postfix.java
+++ STACK_DS/Stack_implementation/Stack_Postfix.java
@@ -1,4 +1,7 @@
-/*Postfix operation using Stack */
+/*Postfix operation in Stack
+Give this example Postfix expression as input: 56+34-*62/+
+The program solves this expression and gives the Output = -8 .
+*/
 
 import java.util.Scanner;
 import java.util.Stack;
@@ -38,8 +41,8 @@ public class Stack_Postfix{
     }
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter your expression: ");
+		System.out.println("Enter your expression: "); // 56+34-*62/+
 		String s=sc.nextLine();
-		System.out.println(postfix(s));
+		System.out.println(postfix(s));//Gives Output = -8
 	}
 }
