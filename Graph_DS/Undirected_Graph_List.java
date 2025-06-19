083ea072cc813295d03c880b29f28ab48819c9c4
Nishanthkb10 <nishanth.kb.10@gmail.com>
2025-06-19 12:13:28 +0530

    Create Undirected_Graph_List.java

---
+++ Graph_DS/Undirected_Graph_List.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class Graph{
+    ArrayList<ArrayList<Integer>> arr=new ArrayList<>();
+    Graph(int v){
+        for(int i=0;i<v;i++){
+            arr.add(new ArrayList<Integer>());
+        }
+    }
+    public void addedge(int u,int v){
+        arr.get(u).add(v);
+        arr.get(v).add(u);
+    }
+    public void display(){
+        for(int i=0;i<arr.size();i++){
+            System.out.print("The vertices connected to "+i+": ");
+            for(int j=0;j<arr.get(i).size();j++){
+                System.out.print(arr.get(i).get(j)+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+public class Undirected_Graph_List
+{
+	public static void main(String[] args) {
+		Graph g=new Graph(5);
+		g.addedge(0,1);
+		g.addedge(0,4);
+		g.addedge(1,2);
+		g.addedge(1,4);
+		g.addedge(2,3);
+		g.addedge(3,4);
+		g.display();
+	}
+}
