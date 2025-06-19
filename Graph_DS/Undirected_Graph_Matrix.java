fae1011bccb0a25dffe7ed88519cac66f90ab6dc
Nishanthkb10 <nishanth.kb.10@gmail.com>
2025-06-19 12:14:51 +0530

    Create Undirected_Graph_Matrix.java

---
+++ Graph_DS/Undirected_Graph_Matrix.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Graph{
+    int[][] matrix;
+    Graph(int v){
+        matrix=new int[v][v];
+    }
+    public void addedge(int u,int v){
+        matrix[u][v]=1;
+        matrix[v][u]=1;
+    }
+    public void display(){
+        for(int i=0;i<matrix.length;i++){
+            System.out.print("The vertices connected to "+i+": ");
+            for(int j=0;j<matrix[i].length;j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+public class Undirected_Graph_Matrix
+{
+	public static void main(String[] args) {
+		Graph g=new Graph(5);
+		g.addedge(0,1);
+		g.addedge(0,4);
+		g.addedge(1,2);
+		g.addedge(1,4);
+		g.addedge(2,3);
+		g.addedge(3,4);
+		g.display();
+	}
+}
